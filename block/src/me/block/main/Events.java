c517c873eccdc4ff056798e568727143ba0150a3
ategus <ronny.schellenberg@gmail.com>
2017-12-05 18:17:16 +0100

    sheduler läuft V1.0

---
+++ block/src/me/block/main/Events.java
@@ -0,0 +1,18 @@
+package me.block.main;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+public class Events implements Listener {
+	
+	public static Plugin plugin;
+	
+	public Events(Plugin plugin) {
+		
+		this.plugin = plugin;
+	}
+	
+	/*
+	@EventHandler
+	public void onBreak(BlockBreakEvent event)*/
+}
---
+++ block/src/me/block/main/main.java
@@ -0,0 +1,39 @@
+package me.block.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class main extends JavaPlugin{
+	
+	@Override
+	public void onEnable(){
+		Bukkit.getConsoleSender().sendMessage("Plugin " + ChatColor.GREEN + "Block" + ChatColor.GREEN + " aktiviert");
+		Bukkit.getPluginManager().registerEvents(new Events(this), this);
+	}
+	@Override
+	public void onDisable(){
+		Bukkit.getConsoleSender().sendMessage("Plugin " + ChatColor.RED + "Block" + ChatColor.RED + " deaktiviert");
+	}
+	
+	
+	@Override
+	public boolean onCommand(final CommandSender sender,Command cmd,String label,String[] args) {
+		
+		if (label.equalsIgnoreCase("test")) {
+			Bukkit.getScheduler().runTaskLater(this, new Runnable(){
+				
+				@Override
+				public void run() {
+					sender.sendMessage("Diese Nachricht kommt nach 5s");
+				}
+			}, 5*20L);
+		}
+		return false;	
+	}
+	
+	
+}
