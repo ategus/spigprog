1905a443d1f68097a78d555432e02e692d86087f
ategus <ronny.schellenberg@gmail.com>
2017-12-07 18:55:30 +0100

    Replace eingefügt

--- block/src/me/block/main/Events.java
+++ block/src/me/block/main/Events.java
@@ -1,7 +1,14 @@
 package me.block.main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.Plugin;
+
+
 
 public class Events implements Listener {
 	
@@ -12,7 +19,21 @@ public class Events implements Listener {
 		this.plugin = plugin;
 	}
 	
-	/*
+	
 	@EventHandler
-	public void onBreak(BlockBreakEvent event)*/
+	public void onBreak(BlockBreakEvent event) {
+		
+		final Block block  = event.getBlock();
+		final Material material = block.getType();
+		
+
+		Bukkit.getScheduler().runTaskLater(plugin, new Runnable(){
+				
+			@Override
+			public void run() {
+				block.setType(material);
+			}
+		}, 3*20L);
+
+	}
 }
